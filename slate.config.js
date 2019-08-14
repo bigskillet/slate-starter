a8c55df0d9083308372c69445a536a220a12c8e8
bigskillet <matt@bigskillet.com>
2019-08-13 20:53:44 -0500

    Added Autoprefixer, reworked slate.config.js

--- slate.config.js
+++ slate.config.js
@@ -3,6 +3,11 @@
 
 const path = require('path');
 
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
   'webpack.extend': {
@@ -13,21 +18,26 @@ module.exports = {
       },
     },
   },
-  'webpack.postcss.plugins': [
-    require('tailwindcss')({
-      theme: {
-        extend: {}
-      },
-      variants: {},
-      plugins: []
-    }),
-    ...process.env.NODE_ENV === 'production'
-      ? [
-        require('cssnano'),
-        require('@fullhuman/postcss-purgecss')({
-          content: ['./src/**/*.html'],
+  'webpack.postcss.plugins': (config) => {
+    const plugins = [
+      tailwindcss({
+        theme: {
+          extend: {}
+        },
+        variants: {},
+        plugins: []  
+      }),
+      autoprefixer
+    ];
+    if (process.env.NODE_ENV === 'production') {
+      plugins.push(
+        purgecss({
+          content: ['./src/**/*.liquid'],
           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-      ] : []
-  ]
+        }),
+        cssnano
+      )
+    }
+    return plugins;
+  }
 };
