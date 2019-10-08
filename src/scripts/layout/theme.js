718755fb061b3450ddf9dfaaf38bfd7fe7be0853
bigskillet <matt@bigskillet.com>
2019-10-08 12:02:21 -0500

    added favicons snippet

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -16,7 +16,7 @@ focusHash();
 bindInPageLinks();
 
 // Apply a specific class to the html element for browser support of cookies.
-if (cookiesEnabled()) {
+if (window.navigator.cookieEnabled) {
   document.documentElement.className = document.documentElement.className.replace(
     'supports-no-cookies',
     'supports-cookies',
