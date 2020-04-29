2a14069039186ab4aa0ce4d75610163f9ef29494
bigskillet <matt@bigskillet.com>
2020-04-29 17:10:24 -0500

    fixed product single variant error

--- slate.config.js
+++ slate.config.js
@@ -3,13 +3,9 @@
 
 const path = require('path');
 
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const purgecss = require('@fullhuman/postcss-purgecss');
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
+  'webpack.babel.enable': false,
   'webpack.extend': {
     resolve: {
       alias: {
@@ -20,24 +16,18 @@ module.exports = {
   },
   'webpack.postcss.plugins': (config) => {
     const plugins = [
-      tailwindcss({
-        theme: {
-          extend: {}
-        },
-        variants: {},
-        plugins: []  
-      }),
-      autoprefixer
+      require('tailwindcss'),
+      require('autoprefixer')
     ];
     if (process.env.NODE_ENV === 'production') {
       plugins.push(
-        purgecss({
+        require('@fullhuman/postcss-purgecss')({
           content: ['./src/**/*.liquid'],
           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
           whitelistPatterns: [],
-          whitelist: []
+          whitelist: ['template-cart']
         }),
-        cssnano({
+        require('cssnano')({
           preset: ['default', {
             discardComments: {
               removeAll: true
